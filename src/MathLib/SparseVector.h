955152d9ca8675188713de62330521971633cf2d
Jonathon Schwartz <Jonathon.schwartz12@gmail.com>
2019-06-06 15:55:35 +1000

    Added printing of belief to sim trace

--- src/MathLib/SparseVector.h
+++ src/MathLib/SparseVector.h
@@ -14,18 +14,17 @@
 using namespace std;
 using namespace momdp;
 
-namespace momdp 
+namespace momdp
 {
 	// SparseVector_Entry should not inherit from MObject, becuase MObject's size is very significant compared to SparseVector_Entry
 	class SparseVector_Entry
 	{
-	public:
-		int index;
-		REAL_VALUE value;
+		public:
+			int index;
+			REAL_VALUE value;
 
-		SparseVector_Entry(void) {}
-		SparseVector_Entry(int _index, REAL_VALUE _value) :	index(_index), value(_value)
-		{}
+			SparseVector_Entry(void) {}
+			SparseVector_Entry(int _index, REAL_VALUE _value) :	index(_index), value(_value) {}
 	};
 
 	class SparseVector : public MObject
@@ -33,124 +32,107 @@ namespace momdp
 		friend class SparseMatrix;
 		friend class DenseVector;
 
-	public:
-		void copyFrom(const SparseVector& x)
-		{
-			this->data = x.data;
-			this->logicalSize = x.logicalSize;
-			this->md5hash = x.md5hash;
-		}
-		vector< SparseVector_Entry > data;
-		void push_back( int index, REAL_VALUE value);
-
-	protected:
-
-		int logicalSize; // the logical length of this sparse vector 
-
-
-
-		void add(SparseVector& result,	const SparseVector& x, 	const SparseVector& y);
-		void subtract(SparseVector& result,	const SparseVector& x, const SparseVector& y);
-
-
-		// Hashing 
-		string md5hash;
-
-	public:
-		SparseVector(void);
-		SparseVector(int _size) { resize(_size); }
-		virtual ~SparseVector(void);
-
-
-		// Sealing
-		void finalize();
-
-
-		// Arithmetc
-		REAL_VALUE getEntropy();
-
-		int argmax();
-		REAL_VALUE operator()( int index) const;
-		REAL_VALUE maskedSum( vector<int> mask);
-		void copyIndex( vector<int>& result);
-		void copyValue( vector<REAL_VALUE>& result);
-
-
-		void operator+=(const SparseVector& x);
-		void operator-=(const SparseVector& x);
-
-		void operator*=(REAL_VALUE s);
-		bool operator==(const SparseVector& x) const;
-
-
-		REAL_VALUE delta(const SparseVector& x) const;
-		REAL_VALUE totalDifference(const SparseVector& x) const;
-		REAL_VALUE totalHashDifference(const SparseVector& x) const;
-
-		int sampleState() const;
-		int argSampleDist() const;
-
-		double norm_inf()
-		{
-			double val, max = 0.0;
-			FOREACH(SparseVector_Entry, xi,  data) 
+		public:
+			void copyFrom(const SparseVector& x)
 			{
-				val = fabs(xi->value);
-				if (val > max) max = val;
+				this->data = x.data;
+				this->logicalSize = x.logicalSize;
+				this->md5hash = x.md5hash;
 			}
-			return max;
-		}
+			vector< SparseVector_Entry > data;
+			void push_back( int index, REAL_VALUE value);
 
-		REAL_VALUE norm_1()
-		{
-			double sum = 0.0;
-			FOREACH(SparseVector_Entry, xi,  data) 
+		protected:
+			int logicalSize; // the logical length of this sparse vector
+			void add(SparseVector& result,	const SparseVector& x, 	const SparseVector& y);
+			void subtract(SparseVector& result,	const SparseVector& x, const SparseVector& y);
+			// Hashing
+			string md5hash;
+
+		public:
+			SparseVector(void);
+			SparseVector(int _size) { resize(_size); }
+			virtual ~SparseVector(void);
+
+			// Sealing
+			void finalize();
+
+			// Arithmetc
+			REAL_VALUE getEntropy();
+
+			int argmax();
+			REAL_VALUE operator()( int index) const;
+			REAL_VALUE maskedSum( vector<int> mask);
+			void copyIndex( vector<int>& result);
+			void copyValue( vector<REAL_VALUE>& result);
+
+			void operator+=(const SparseVector& x);
+			void operator-=(const SparseVector& x);
+			void operator*=(REAL_VALUE s);
+			bool operator==(const SparseVector& x) const;
+
+			REAL_VALUE delta(const SparseVector& x) const;
+			REAL_VALUE totalDifference(const SparseVector& x) const;
+			REAL_VALUE totalHashDifference(const SparseVector& x) const;
+
+			int sampleState() const;
+			int argSampleDist() const;
+
+			double norm_inf()
 			{
-				sum += fabs(xi->value);
+				double val, max = 0.0;
+				FOREACH(SparseVector_Entry, xi,  data)
+				{
+					val = fabs(xi->value);
+					if (val > max) max = val;
+				}
+				return max;
 			}
-			return sum;
-		}
 
-		double norm_2()
-		{
-			double sum = 0.0;
-			FOREACH(SparseVector_Entry, xi,  data) 
+			REAL_VALUE norm_1()
 			{
-				sum += (xi->value)*(xi->value);
+				double sum = 0.0;
+				FOREACH(SparseVector_Entry, xi,  data)
+				{
+					sum += fabs(xi->value);
+				}
+				return sum;
 			}
-			return sum;
-		}
 
+			double norm_2()
+			{
+				double sum = 0.0;
+				FOREACH(SparseVector_Entry, xi,  data)
+				{
+					sum += (xi->value)*(xi->value);
+				}
+				return sum;
+			}
 
-		bool isDifferentByAtLeastSingleEntry(const SparseVector& x, const REAL_VALUE& threshold) const;
+			bool isDifferentByAtLeastSingleEntry(const SparseVector& x, const REAL_VALUE& threshold) const;
 
-		// Capacity
-		int size(void) const { return logicalSize; }
-		int filled(void) const { return data.size(); }
+			// Capacity
+			int size(void) const { return logicalSize; }
+			int filled(void) const { return data.size(); }
 
-		void resize( int _size);
+			void resize( int _size);
 
-		//void push_back( int index, REAL_VALUE value);
-		//void canonicalize(void) {}
+			//void push_back( int index, REAL_VALUE value);
+			//void canonicalize(void) {}
 
+			// IO
+			void read(std::istream& in);
+			std::ostream& write(std::ostream& out) const;
 
-		// IO
-		void read(std::istream& in);
-		std::ostream& write(std::ostream& out) const;
+			//void dumpSparse(std::ostream& out) const;
+			//void dumpDense(std::ostream& out, int numStates) const;
 
-		//void dumpSparse(std::ostream& out) const;
-		//void dumpDense(std::ostream& out, int numStates) const;
+			string md5HashValue();
+			string ToString() const;
 
-
-
-
-		string md5HashValue();
-		string ToString() const;
-
-		string convToString(unsigned char *bytes);
-		string getHashFromSparseVectorTruncated(SparseVector& x);
-		string getHashFromCVector(SparseVector& x);
-
+			string convToString(unsigned char *bytes);
+			string getHashFromSparseVectorTruncated(SparseVector& x);
+			string getHashFromCVector(SparseVector& x);
 	};
 
 	// result = x .* y [for all i, result(i) = x(i) * y(i)]
@@ -158,11 +140,15 @@ namespace momdp
 	{
 		double sum = 0.0;
 		U yi = ybegin;
-		for (T xi = xbegin; xi != xend; xi++) {
-			while (1) {
+		for (T xi = xbegin; xi != xend; xi++)
+		{
+			while (1)
+			{
 				if (yi == yend) return sum;
-				if (yi->index >= xi->index) {
-					if (yi->index == xi->index) {
+				if (yi->index >= xi->index)
+				{
+					if (yi->index == xi->index)
+					{
 						sum += xi->value * yi->value;
 					}
 					break;
@@ -172,8 +158,6 @@ namespace momdp
 		}
 		return sum;
 	}
-
 }
-
 
 #endif
--- src/Simulator/SimulationEngine.h
+++ src/Simulator/SimulationEngine.h
@@ -9,42 +9,34 @@ using namespace momdp;
 
 namespace momdp
 {
-    class AlphaVectorPolicy;
-    class SolverParams;
+  class AlphaVectorPolicy;
+  class SolverParams;
 
-    class SimulationEngine
-    {
-        private:
-            SharedPointer<MOMDP> problem;
-            SharedPointer<AlphaVectorPolicy> policy;
-            SolverParams * solverParams;
+  class SimulationEngine
+  {
+    private:
+      SharedPointer<MOMDP> problem;
+      SharedPointer<AlphaVectorPolicy> policy;
+      SolverParams * solverParams;
 
+    public:
+      SimulationEngine();
+      void setup(SharedPointer<MOMDP> problem, SharedPointer<AlphaVectorPolicy> policy, SolverParams * solverParams);
+      void performActionObs(belief_vector& outBelObs, int action, const BeliefWithState& belSt) const;
+      void performActionUnobs(belief_vector& outBelUnobs, int action, const BeliefWithState& belSt, int currObsState) const;
+      void getPossibleObservations(belief_vector& possObs, int action, const BeliefWithState& belSt) const;
+      string toString();
 
-        public:
-            SimulationEngine();
-            void setup(SharedPointer<MOMDP> problem, SharedPointer<AlphaVectorPolicy> policy, SolverParams * solverParams);
-            void performActionObs(belief_vector& outBelObs, int action, const BeliefWithState& belSt) const;
-            void performActionUnobs(belief_vector& outBelUnobs, int action, const BeliefWithState& belSt, int currObsState) const;
-            void getPossibleObservations(belief_vector& possObs, int action, const BeliefWithState& belSt) const;
-            string toString();
+      double getReward(const BeliefWithState& belst, int action);
 
-            double getReward(const BeliefWithState& belst, int action);
+      void checkTerminal(string o, string s, vector<int> &bhout, vector<int> &fhout);
+      int getGreedyAction(vector<int> &, vector<int> &);
 
-            void checkTerminal(string o, string s, vector<int> &bhout, vector<int> &fhout);
-            int getGreedyAction(vector<int> &, vector<int> &);
+      void display(belief_vector& b, ostream& s);
+      void printCurrentBeliefVec(belief_vector& b, ostream& streamOut);
+      int runFor(int iters, ofstream* streamOut, double& reward, double& expReward);
 
-
-
-            void display(belief_vector& b, ostream& s);
-            int runFor(int iters, ofstream* streamOut, double& reward, double& expReward);
-
-
-
-            virtual ~SimulationEngine(void);
-    };
-
-
-
+      virtual ~SimulationEngine(void);
+  };
 }
 #endif
-
